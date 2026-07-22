41b717a1967e372b677a7ec6f51228e5c33e6a2c
Daniel Okoronkwo <danielokoronkwo90@gmail.com>
2026-07-22 05:10:40 +0100

    chore(ts): ring buffer  implementation in typescript

--- data-structures/ring-buffer/implementations/ring-buffer.ts
+++ data-structures/ring-buffer/implementations/ring-buffer.ts
@@ -2,6 +2,13 @@ import { RingBuffer } from "../interfaces/ring-buffer";
 
 export class MyRingBuffer<T> implements RingBuffer<T> {
   private readonly initialCapacity: number;
+  private container: Array<T>
+  private head: number;
+  private tail: number;
+
+  // bookeeping
+  private currentLength: number
+
 
   public constructor(initialCapacity = 4) {
     if (initialCapacity <= 0) {
@@ -9,49 +16,79 @@ export class MyRingBuffer<T> implements RingBuffer<T> {
     }
 
     this.initialCapacity = initialCapacity;
+    this.container = new Array(initialCapacity);
+    this.head = 0;
+    this.tail = 0
+    this.currentLength = 0
+
   }
 
   public length(): number {
-    throw new Error("TODO: implement length");
+    return this.currentLength
   }
 
   public capacity(): number {
-    throw new Error("TODO: implement capacity");
+    return this.initialCapacity
   }
 
   public isEmpty(): boolean {
-    throw new Error("TODO: implement isEmpty");
+    return this.length() <= 0
   }
 
   public isFull(): boolean {
-    throw new Error("TODO: implement isFull");
+    return this.length() === this.capacity()
   }
 
   public front(): T | undefined {
-    throw new Error("TODO: implement front");
+    if(this.isEmpty()) return undefined
+    return this.container[this.head]
   }
 
   public rear(): T | undefined {
-    throw new Error("TODO: implement rear");
+    if(this.isEmpty()) return undefined;
+    let rearIndex = (this.tail - 1 + this.capacity()) % this.capacity()
+    return this.container[rearIndex]
   }
 
   public enqueue(_value: T): boolean {
-    throw new Error("TODO: implement enqueue");
+    if(this.isFull()) return false
+    
+    this.container[this.tail] = _value
+    this.currentLength++
+    this.tail = (this.tail + 1) % this.getInitialCapacity()
+
+    return true
   }
 
   public dequeue(): T | undefined {
-    throw new Error("TODO: implement dequeue");
+    if(this.isEmpty()) return undefined
+    const val = this.container[this.head]
+    this.head = (this.head + 1) % this.getInitialCapacity()
+    this.currentLength--
+    return val
   }
 
   public clear(): void {
-    throw new Error("TODO: implement clear");
+    this.container = new Array(this.initialCapacity)
+    this.currentLength = 0
   }
 
   public toArray(): T[] {
-    throw new Error("TODO: implement toArray");
+   let result = [] as T[]
+
+   for(let i = 0; i < this.length(); i++) {
+    let idx = (this.head + i) % this.capacity()
+    const v = this.container[idx]
+    if(v && v !== undefined) {
+      result.push(v)
+    }
+   }
+
+   return result
   }
 
   protected getInitialCapacity(): number {
     return this.initialCapacity;
   }
 }
+
